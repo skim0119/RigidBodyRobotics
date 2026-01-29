6215446dc373351535f02b29ec276d4a12fc727d
Seung Hyun Kim <skim0119@gmail.com>
2026-01-28 18:49:13 -0600

    distinguish symplectic stepper and explicit stepper

--- elastica_rigid/timestepper/explicit_stepper.py
+++ elastica_rigid/timestepper/explicit_stepper.py
@@ -4,7 +4,7 @@ from typing import Protocol, TYPE_CHECKING, Any, Callable
 
 from itertools import zip_longest
 
-from elastica.systems.protocol import SystemProtocol
+from elastica.systems.protocol import SymplecticSystemProtocol
 from elastica.typing import (
     SystemCollectionType,
     SystemType,
@@ -15,14 +15,9 @@ from elastica.typing import (
 import numpy as np
 
 
-class RigidSystem(SystemProtocol, Protocol):
+class RigidSystem(SymplecticSystemProtocol, Protocol):
     """
     """
-
-    def update_state(self, time, dt): 
-        """
-        Update state
-        """
 
     def emplace_back_state(self):
         """
@@ -36,10 +31,6 @@ class ExplicitEulerForward(:
     """
     Explicit Euler forward stepper for differential-algebraic equations.
     """
-
-    def __init__(self):
-        # placeholder for stepping information
-        self.info: dict[str, Any] = {}
 
     def step(
         self,
@@ -64,7 +55,9 @@ class ExplicitEulerForward(:
 
         # Step
         for system in SystemCollection.final_systems():
-            system.update_state(time, dt)
+            system.update_accelerations(time)
+            system.update_kinematics(time, dt)
+            system.update_dynamics(time, dt)
 
         SystemCollection.constrain_values(simulation_time)
         SystemCollection.constrain_rates(simulation_time)
@@ -88,5 +81,67 @@ class ExplicitEulerForward(:
         (The function is used for single system instance, mainly for testing purposes.)
         """
 
-        system.update_state(time, dt)
+        system.update_accelerations(time)
+        system.update_kinematics(time, dt)
+        system.update_dynamics(time, dt)
+        return time + dt
+
+
+class SymplecticEulerForward(:
+    """
+    Explicit Euler forward stepper for differential-algebraic equations.
+    """
+
+    def step(
+        self,
+        SystemCollection: SystemCollectionType,
+        time: np.float64,
+        dt: np.float64,
+    ) -> np.float64:
+        """
+        Function for doing symplectic stepper over the user defined rods (system).
+
+        Returns
+        -------
+        time: float
+            The time after the integration step.
+
+        """
+        simulation_time = np.float64(time)
+        simulation_dt = np.float64(dt)
+
+        # Compute external forces and couples
+        SystemCollection.synchronize(simulation_time)
+
+        # Step
+        for system in SystemCollection.final_systems():
+            system.update_accelerations(time)
+            system.update_dynamics(time, dt)
+            system.update_kinematics(time, dt)
+
+        SystemCollection.constrain_values(simulation_time)
+        SystemCollection.constrain_rates(simulation_time)
+        SystemCollection.apply_callbacks(
+            simulation_time, round(simulation_time / simulation_dt)
+        )
+
+        # Zero out the external forces and torques
+        for system in SystemCollection.final_systems():
+            system.zeroed_out_external_forces_and_torques(simulation_time)
+
+        return time + dt
+
+    def step_single_instance(
+        self,
+        system: RigidSystem,
+        time: np.float64,
+        dt: np.float64,
+    ) -> np.float64:
+        """
+        (The function is used for single system instance, mainly for testing purposes.)
+        """
+
+        system.update_accelerations(time)
+        system.update_dynamics(time, dt)
+        system.update_kinematics(time, dt)
         return time + dt
