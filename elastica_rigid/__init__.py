7d2fd0269822dccb3d5dc2f38ef987c9cbc2f2df
Seung Hyun Kim <skim0119@gmail.com>
2026-01-28 22:41:22 -0600

    update energy computation

--- elastica_rigid/__init__.py
+++ elastica_rigid/__init__.py
@@ -1,4 +1,10 @@
 from .body.roomba import Roomba
 from .timestepper.explicit_stepper import ExplicitEulerForward
 from .timestepper.symplectic_stepper import SymplecticEulerForward
-from .external_forces import ConstantForce, PotentialFieldForce
+from .external_forces import (
+    ConstantForce,
+    PotentialFieldForce,
+    WheelForceSequence,
+    EnvironmentForces2D,
+)
+from .visualize.robot_on_field import Visualize
