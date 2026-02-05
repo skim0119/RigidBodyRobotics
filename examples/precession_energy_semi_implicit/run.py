114092686dc7f5a161f075d1f2887e5daf4478fb
Seung Hyun Kim <skim0119@gmail.com>
2026-02-05 12:55:33 -0600

    increase sim-time to test energy stability with sinusoidal input

--- examples/precession_energy_semi_implicit/run.py
+++ examples/precession_energy_semi_implicit/run.py
@@ -7,7 +7,7 @@ import elastica as ea
 import elastica_rigid as er
 
 
-def run(stepper, T=10.0, dt=0.01):
+def run(stepper, T=100.0, dt=0.01):
     sphere = er.Sphere(center=np.zeros(3), base_radius=1, density=1e3)
     sphere.omega_collection[:] = np.array(
         [[1.0], [1.0], [0.5]]
@@ -30,6 +30,9 @@ def run(stepper, T=10.0, dt=0.01):
     time = 0.0
     total_steps = int(T / dt)
     for tidx in tqdm(range(total_steps)):
+        sphere.external_torques[:] = np.sin(np.pi * tidx * dt)
+        isphere.external_torques[:] = np.sin(np.pi * tidx * dt)
+
         stepper.step_single_instance(sphere, time, dt)
         time = stepper.step_single_instance(isphere, time, dt)
 
@@ -60,7 +63,7 @@ ienergy = np.array(results["energy_sphere_implicit"]).squeeze()
 
 fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(111)
-ax.plot(time, energy, label="original explicit lagrange transport")
+# ax.plot(time, energy, label="original explicit lagrange transport")
 ax.plot(time, ienergy, label="implicit mid-omega for lagrange transport")
 ax.legend()
 plt.savefig("energy.png")
