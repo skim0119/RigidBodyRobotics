5ba8ce445dd4154438510608eb2664c00bd72235
Seung Hyun Kim <skim0119@gmail.com>
2026-02-05 00:15:03 -0600

    sphere with semi-implicit accelerator

--- elastica_rigid/__init__.py
+++ elastica_rigid/__init__.py
@@ -1,5 +1,6 @@
 from .body.roomba import Roomba
 from .body.sphere import Sphere
+from .body.implicit_sphere import SphereImplicit
 from .timestepper.explicit_stepper import ExplicitEulerForward
 from .timestepper.symplectic_stepper import SymplecticEulerForward
 from .external_forces import (
