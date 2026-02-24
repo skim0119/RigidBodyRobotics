14b373f5a11a422b649e519bd52cb3d31e2a533c
Seung Hyun Kim <skim0119@gmail.com>
2026-02-24 15:08:54 -0600

    add simple policy for testing

---
+++ examples/se3_control_sandbox/policy/__init__.py
@@ -0,0 +1,7 @@
+from .pose_tracking import ControlConfig, DEFAULT_CONTROL_CONFIG, SimplePoseTracking
+
+__all__ = [
+    "ControlConfig",
+    "DEFAULT_CONTROL_CONFIG",
+    "SimplePoseTracking",
+]
